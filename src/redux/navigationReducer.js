1caa601bf67b3a52942c82f7d6fd0a97de858c5a
Sergey <avtsynov.s@yandex.ru>
2022-01-09 02:25:15 +0300

    update users

--- src/redux/navigationReducer.js
+++ src/redux/navigationReducer.js
@@ -2,45 +2,45 @@ let initialState = {
   users: [
     {
       id: 1,
-      name: "Alex",
-      secondName: "Megos",
+      name: 'Alex',
+      secondName: 'Megos',
       avatar:
-        "https://picjumbo.com/wp-content/uploads/alone-with-his-thoughts-1080x720.jpg",
+        'https://picjumbo.com/wp-content/uploads/alone-with-his-thoughts-1080x720.jpg',
     },
     {
       id: 2,
-      name: "Ann",
-      secondName: "Willson",
+      name: 'Ann',
+      secondName: 'Willson',
       avatar:
-        "https://cdn.pixabay.com/photo/2014/05/03/00/56/summerfield-336672_960_720.jpg",
+        'https://anime-fans.ru/wp-content/uploads/2021/04/Skachat-milye-kartinki-anime-na-avu_19.jpg',
     },
     {
       id: 3,
-      name: "Jack",
-      secondName: "London",
+      name: 'Jack',
+      secondName: 'London',
       avatar:
-        "https://cdn.pixabay.com/photo/2015/06/08/15/02/pug-801826_960_720.jpg",
+        'https://www.meme-arsenal.com/memes/90bc67357ee4ac90b42daa287119ace2.jpg',
     },
     {
       id: 4,
-      name: "Thomas",
-      secondName: "Katcher",
+      name: 'Thomas',
+      secondName: 'Katcher',
       avatar:
-        "https://cdn.pixabay.com/photo/2016/01/19/17/57/car-1149997_960_720.jpg",
+        'https://cdn.pixabay.com/photo/2016/01/19/17/57/car-1149997_960_720.jpg',
     },
     {
       id: 5,
-      name: "Jessica",
-      secondName: "Phils",
+      name: 'Jessica',
+      secondName: 'Phils',
       avatar:
-        "https://cdn.pixabay.com/photo/2016/01/19/17/48/woman-1149911_960_720.jpg",
+        'https://goodwall.ru/image/picture/1204-1280x960-fbb72d1c78b3ee11e97c6ef2729b26ae.jpg',
     },
     {
       id: 6,
-      name: "Mia",
-      secondName: "Li",
+      name: 'Mia',
+      secondName: 'Li',
       avatar:
-        "https://cdn.pixabay.com/photo/2015/03/26/09/39/woman-690034_960_720.jpg",
+        'https://img2.akspic.ru/originals/1/5/9/5/4/145951-anime-zvukovoe_oborudovanie-krasnyj_cvet-multfilm-art-1920x1080.jpg',
     },
   ],
 };
